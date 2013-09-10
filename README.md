8c17879299e07d6c800e0a1af98bd61ca5fcd784
David Rocamora <david.rocamora@controlgroup.com>
2013-09-10 14:13:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+cfn-summarize
+======
+
+**Summarizes a CloudFormation template.**
+
+This prints a list of the things in a CloudFormation template:
+
+    cfn-summarize [-a] template1
+    
+The ```-a``` flag will show acme addresses to the item. Probably only useful to @drocamor.
---
+++ cfn-summarize.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type CloudFormationOutput struct {
+	Description string
+}
+
+type CloudFormationParameter struct {
+	Default     string
+	Description string
+	Type        string
+}
+
+type CloudFormationResource struct {
+	Type string
+}
+
+type CloudFormationTemplate struct {
+	AWSTemplateFormatVersion string
+	Description              string
+	Mappings                 map[string]interface{}
+	Outputs                  map[string]CloudFormationOutput
+	Parameters               map[string]CloudFormationParameter
+	Resources                map[string]CloudFormationResource
+}
+
+var acme bool
+
+func main() {
+	// This is a feature that is probably only useful to @drocamor
+	flag.BoolVar(&acme, "a", false, "Show acme addresses of items")
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Println("You must provide one CloudFormation template to summarize.")
+		os.Exit(1)
+	}
+
+	file, e := ioutil.ReadFile(flag.Arg(0))
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+
+	var template CloudFormationTemplate
+	json.Unmarshal(file, &template)
+
+	fmt.Printf("Description: %s\n\n", template.Description)
+
+	if template.Mappings != nil {
+		fmt.Printf("Mappings:\n\n")
+		for k, _ := range template.Mappings {
+			fmt.Printf("%s\n", k)
+			if acme {
+				fmt.Printf(`%s:/%s\"[\ ]*:/`, flag.Arg(0), k)
+				fmt.Printf("\n")
+			}
+			fmt.Printf("\n")
+		}
+	}
+
+	if template.Outputs != nil {
+		fmt.Printf("Outputs:\n\n")
+		for k, v := range template.Outputs {
+			fmt.Printf("%s\n", k)
+			if acme {
+				fmt.Printf(`%s:/%s\"[\ ]*:/`, flag.Arg(0), k)
+				fmt.Printf("\n")
+			}
+			fmt.Printf("Description: %s\n", v.Description)
+			fmt.Printf("\n")
+		}
+	}
+
+	if template.Parameters != nil {
+		fmt.Printf("Parameters:\n\n")
+		for k, v := range template.Parameters {
+			fmt.Printf("%s\n", k)
+			if acme {
+				fmt.Printf(`%s:/%s\"[\ ]*:/`, flag.Arg(0), k)
+				fmt.Printf("\n")
+			}
+			fmt.Printf("Default: %s\n", v.Default)
+			fmt.Printf("Description: %s\n", v.Description)
+			fmt.Printf("Type: %s\n", v.Type)
+
+			fmt.Printf("\n")
+		}
+	}
+
+	if template.Resources != nil {
+		fmt.Printf("Resources:\n\n")
+		for k, v := range template.Resources {
+			fmt.Printf("%s\n", k)
+			if acme {
+				fmt.Printf(`%s:/%s\"[\ ]*:/`, flag.Arg(0), k)
+				fmt.Printf("\n")
+			}
+			fmt.Printf("Type: %s\n", v.Type)
+			fmt.Printf("\n")
+		}
+	}
+
+}
